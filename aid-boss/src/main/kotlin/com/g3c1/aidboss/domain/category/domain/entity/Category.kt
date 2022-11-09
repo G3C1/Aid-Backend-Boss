f2b14a60406f450beb2aee4ff602cdb8378903f1
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-11-09 22:43:26 +0900

    :recycle: entityType
    똑같이 해버리기

--- aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/category/domain/entity/Category.kt
+++ aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/category/domain/entity/Category.kt
@@ -1,11 +1,17 @@
 package com.g3c1.aidboss.domain.category.domain.entity
 
+import com.g3c1.aidboss.domain.store.domain.entity.Store
 import com.g3c1.temiuser.global.entity.BaseIdEntity
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 @Entity
 data class Category(
     @Column(nullable = false)
-    val name: String
+    val name: String,
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    val store: Store
 ):BaseIdEntity()
---
+++ aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/store/domain/entity/Store.kt
@@ -0,0 +1,19 @@
+package com.g3c1.aidboss.domain.store.domain.entity
+
+import com.g3c1.aidboss.domain.user.domain.entity.User
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+
+@Entity
+data class Store(
+    @Id
+    @Column(name= "serial_number")
+    val serialNumber:Long,
+    val name: String,
+    val description: String,
+    val img: String,
+    @ManyToOne
+    val user:User
+)
