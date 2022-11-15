ee56c19ab380183c4219ce3473e26b748f6c8a53
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-11-15 13:35:52 +0900

    :recycle: Valid 추가
    ㅇㅇ

--- aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/store/presentation/StoreController.kt
+++ aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/store/presentation/StoreController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("api/v2/store")
@@ -19,7 +20,7 @@ class StoreController(
     private val storeConverter: StoreConverter
 ) {
     @PostMapping
-    fun createStore(@RequestBody createStoreRequest: CreateStoreRequest): ResponseEntity<Void> =
+    fun createStore(@Valid @RequestBody createStoreRequest: CreateStoreRequest): ResponseEntity<Void> =
         storeConverter.toDto(createStoreRequest)
             .let { storeService.createStore(it) }
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
--- aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/store/presentation/data/request/CreateStoreRequest.kt
+++ aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/store/presentation/data/request/CreateStoreRequest.kt
@@ -1,8 +1,14 @@
 package com.g3c1.aidboss.domain.store.presentation.data.request
 
+import org.jetbrains.annotations.NotNull
+
 data class CreateStoreRequest (
+    @field:NotNull
     val serialNumber: Long,
+    @field:NotNull
     val name: String,
+    @field:NotNull
     val description: String,
+    @field:NotNull
     val img: String
 )
