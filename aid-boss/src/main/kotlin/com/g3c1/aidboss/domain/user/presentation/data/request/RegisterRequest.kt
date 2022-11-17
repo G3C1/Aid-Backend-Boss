a86abf7a71f9d05e8ecb10bc09faeeb7aedd4d5a
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-11-18 08:04:57 +0900

    :recycle: 정규식
    정규햄

--- aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/user/presentation/data/request/RegisterRequest.kt
+++ aid-boss/src/main/kotlin/com/g3c1/aidboss/domain/user/presentation/data/request/RegisterRequest.kt
@@ -7,10 +7,9 @@ data class RegisterRequest (
     @field:NotNull
     val id: String,
     @field:NotNull
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-        message = "비밀번호에 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+    @field:Pattern(regexp = "^.(?=^.{8,20}\$)(?=.\\d)(?=.[a-zA-Z])(?=.[!@#\$%^&+=]).*\$")
     val password: String,
     @field:NotNull
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @field:Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$")
     val name:String
 )
