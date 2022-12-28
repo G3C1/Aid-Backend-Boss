af5766416168d108cc7a4c67a3797d57a76a5bd6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-28 16:10:30 +0900

    :heavy_plus_sign: s3 의존성 추가
    ㅇㅇ

--- aid-boss/build.gradle.kts
+++ aid-boss/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
